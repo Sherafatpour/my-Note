fd11ca1a0b86a0bbdca6bb30fa7202d99ef922b8
red <red@red.red>
2020-08-13 16:22:47 +0430

    create Note entity

---
+++ app/src/main/java/name/sherafatpour/mynotepad/Note.kt
@@ -0,0 +1,10 @@
+package name.sherafatpour.mynotepad
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_table")
+class Note(val title: String, val description: String, val priority: Int) {
+    @PrimaryKey(autoGenerate = true)
+    var id = 0
+}
