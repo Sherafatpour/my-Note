619cf5259ccc29624e4dbb834b49f4ef077cee45
red <red@red.red>
2020-08-19 21:59:39 +0430

    create RecyclerView Item Touch Helper  (swipe item)

--- app/src/main/java/name/sherafatpour/mynotepad/MainActivity.kt
+++ app/src/main/java/name/sherafatpour/mynotepad/MainActivity.kt
@@ -3,11 +3,14 @@ package name.sherafatpour.mynotepad
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.observe
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,11 +24,28 @@ class MainActivity : AppCompatActivity() {
         noteRecyclerview.setHasFixedSize(true)
         val adapter = NoteAdapter()
         noteRecyclerview.adapter = adapter
+
+       ItemTouchHelper( object : ItemTouchHelper.SimpleCallback(0 , ItemTouchHelper.LEFT or  ItemTouchHelper.RIGHT ){
+           override fun onMove(
+               recyclerView: RecyclerView,
+               viewHolder: RecyclerView.ViewHolder,
+               target: RecyclerView.ViewHolder
+           ): Boolean {
+               return false
+           }
+
+           override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+               Toast.makeText(this@MainActivity,"Note Removed",Toast.LENGTH_LONG).show()
+               noteViewModel.delete(adapter.getNoteAt(viewHolder.adapterPosition))
+
+           }
+
+       }).attachToRecyclerView(noteRecyclerview)
+
         noteViewModel.getAllNote()!!.observe(this) { notesList ->
 
             adapter.setNote(notesList)
-
-
 
         }
 
